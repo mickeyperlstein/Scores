ea0a361a57b6838060a8d8c039fabf0413dabdce
Mickey <corp.mp@gmail.com>
2016-02-28 20:19:06 +0200

    Added WebApi on the side

---
+++ WebApplication1/Controllers/GameController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class GameController : ApiController
+    {
+        private ScoresDBEntities1 db = new ScoresDBEntities1();
+
+        // GET api/Game
+        public IQueryable<Game> GetGames()
+        {
+            return db.Games;
+        }
+
+        // GET api/Game/5
+        [ResponseType(typeof(Game))]
+        public IHttpActionResult GetGame(int id)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+
+        // PUT api/Game/5
+        public IHttpActionResult PutGame(int id, Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != game.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(game).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Game
+        [ResponseType(typeof(Game))]
+        public IHttpActionResult PostGame(Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Games.Add(game);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = game.Id }, game);
+        }
+
+        // DELETE api/Game/5
+        [ResponseType(typeof(Game))]
+        public IHttpActionResult DeleteGame(int id)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            db.Games.Remove(game);
+            db.SaveChanges();
+
+            return Ok(game);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GameExists(int id)
+        {
+            return db.Games.Count(e => e.Id == id) > 0;
+        }
+    }
+}
