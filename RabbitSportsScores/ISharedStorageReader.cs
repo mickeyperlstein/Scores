643442c032ee1117cc03b39ddb1a528ab2a65bb6
Mickey Perlstein <corp.mp@gmail.com>
2016-03-06 17:24:25 +0200

    Added files via upload

---
+++ RabbitSportsScores/ISharedStorageReader.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using RabbitSportsScores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Storage
+{
+    public interface ISharedStorageReader : IStorageConnector
+    {
+        event Action<string> RecievedMessage;
+        void StartRecieve();
+    }
+}
