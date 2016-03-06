392dbf4450ba50d2a945a6484ca3a84248e49809
Mickey Perlstein <corp.mp@gmail.com>
2016-03-06 17:25:17 +0200

    Added files via upload

---
+++ RabbitSportsScores/RabbitStorageReader.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitSportsScores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Storage.Rabbit
+{
+    class RabbitStorageReader : RabbitMQConnector, ISharedStorageReader
+    {
+        private EventingBasicConsumer consumer;
+        public event Action<string> RecievedMessage;
+       
+        protected virtual void OnRecievedMessage(string json)
+        {
+ 	        if (this.RecievedMessage!=null)
+                this.RecievedMessage(json);
+        }
+
+                
+        public void StartRecieve()
+        {
+
+            this.consumer = new EventingBasicConsumer(this.channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var json = Encoding.UTF8.GetString(body);
+
+                OnRecievedMessage(json);
+
+            };
+        }
+    }
+}
